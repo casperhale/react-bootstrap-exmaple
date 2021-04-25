40323d15a0c1ce7ed7580de2253e45851a3e0029
belikovdev <belikovdev99@gmail.com>
2021-04-25 16:45:02 +0300

    add modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
+import { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ModalForm from './ModalForm'
 
 function App() {
+  const [show, setShow] = useState(false)
+
+  const handleCreateClick = (e, userData) => {
+    console.log('create user logic')
+    console.log(userData)
+    // make a request to create user
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={1}>
+          <Button onClick={() => setShow(true)}>Click</Button>
+          <ModalForm show={show} onHide={() => setShow(false)} onCreateClick={handleCreateClick} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
